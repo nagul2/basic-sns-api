2d5bd9ca88003196d6d0e9b4117cfe4b5cadbde4
pjm529 <pjm529529@gmail.com>
2025-04-07 19:49:01 +0900

    Feat/#4 Users Entity 생성

--- src/main/java/com/sns/api/users/domain/entity/Users.java
+++ src/main/java/com/sns/api/users/domain/entity/Users.java
@@ -4,13 +4,32 @@ package com.sns.api.users.domain.entity;
 import com.sns.api.common.domain.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDate;
 
 @Entity
 @Getter
-@Table
+@Table(name = "users")
+@SQLRestriction("is_deleted = false")
 public class Users extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // PK
+
+    @Column(nullable = false, unique = true)
+    private String email; // 이메일
+
+    @Column(nullable = false)
+    private String username; // 이름
+
+    @Column(nullable = false)
+    private String password; // 비밀번호
+
+    @Column
+    private LocalDate birth; // 생년월일
+
+    @Column(nullable = false)
+    private Boolean isDeleted; // 삭제여부
 }
