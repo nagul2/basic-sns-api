9698b4229279165676fd77dc495408acebc335c4
SeungWoo Ryu <swstar21c@gmail.com>
2025-04-08 12:49:54 +0900

    Feat/#13: entity 및 repository 구현
    - JPA에서 soft delete를 구현하기 위해 `@SQLDelete` 어노테이션 사용 (delete 메서드 호출 시 실행됨)
    - 추후 좋아요 수 등의 필드 추가할 예정

--- src/main/java/com/sns/api/posts/repository/PostsRepository.java
+++ src/main/java/com/sns/api/posts/repository/PostsRepository.java
@@ -1,4 +1,7 @@
 package com.sns.api.posts.repository;
 
-public interface PostsRepository {
+import com.sns.api.posts.domain.entity.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
