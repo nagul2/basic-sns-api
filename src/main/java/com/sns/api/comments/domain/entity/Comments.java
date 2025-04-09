adf9fb2365f92b6c35d5af543272b23cab553761
SeungWoo Ryu <swstar21c@gmail.com>
2025-04-09 15:06:43 +0900

    Feat/#23: 댓글 entity 및 repository 구현
    - 댓글은 특정 게시물에 속해야만 한다.
    - 특정 게시물에 속하는 댓글들을 조회하는 JPA 메서드 선언

--- src/main/java/com/sns/api/comments/domain/entity/Comments.java
+++ src/main/java/com/sns/api/comments/domain/entity/Comments.java
@@ -1,4 +1,49 @@
 package com.sns.api.comments.domain.entity;
 
-public class Comments {
+import com.sns.api.common.domain.entity.BaseUserEntity;
+import com.sns.api.posts.domain.entity.Posts;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE comments SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+public class Comments extends BaseUserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;            // PK
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Posts post;         // 소속 게시물
+    
+    @Column(nullable = false)
+    private String content;     // 댓글 본문
+
+    @Column(nullable = false)
+    private Boolean isDeleted;  // 삭제 여부
+
+    public static Comments of(Posts post, String content) {
+        return new Comments(post, content);
+    }
+
+    public Comments(Posts post, String content) {
+        this.post = post;
+        this.content = content;
+        this.isDeleted = false;
+    }
+
+    public void updateComment(String content) {
+        this.content = content;
+    }
+
 }
