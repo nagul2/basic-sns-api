9cb713a7642e8fc840d5c1ef46d13059694b198b
SeungWoo Ryu <swstar21c@gmail.com>
2025-04-09 23:50:53 +0900

    Feat/#35: 댓글 조회 시 발생하는 N + 1 문제 해결
    - 기존 코드 방식은 N + 1 문제를 가지고 있었다.
       - 댓글 목록 조회 (쿼리 1회 실행)
       - 각 댓글의 작성자 정보 조회 (댓글 N개 만큼 쿼리 N회 실행)
       - => 총 N + 1번만큼 실행됨
    - EntityGraph를 사용하면 attributePaths 의 필드를 fetch join 해서 가져온다. 즉, 추가로 쿼리를 날리지 않아도 된다.

--- src/main/java/com/sns/api/comments/repository/CommentsRepository.java
+++ src/main/java/com/sns/api/comments/repository/CommentsRepository.java
@@ -4,10 +4,18 @@ import com.sns.api.comments.domain.entity.Comments;
 import com.sns.api.posts.domain.entity.Posts;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
 
+    /**
+     * 
+     * @param post 게시물 entity
+     * @param pageable 생성일 기준 내림차순 정렬이 default
+     * @return
+     */
+    @EntityGraph(attributePaths = "createdBy")
     Page<Comments> findAllByPost(Posts post, Pageable pageable);
 
 }
