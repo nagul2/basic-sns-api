d4b5b31edf6bab038b6662ceb8033f6239ac56c2
pjm529 <pjm529529@gmail.com>
2025-04-10 11:39:23 +0900

    Feat/#44 Follows Entity 설계

---
+++ src/main/java/com/sns/api/follow/domain/entity/Follows.java
@@ -0,0 +1,38 @@
+package com.sns.api.follow.domain.entity;
+
+import com.sns.api.users.domain.entity.Users;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "follows", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"follower_id", "following_id"})
+})
+@Getter
+@NoArgsConstructor
+public class Follows {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id", updatable = false, nullable = false)
+    private Users follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id", updatable = false, nullable = false)
+    private Users following;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
