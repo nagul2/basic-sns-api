43b3f6e6db3550970d9600fdcfc24bc0ab15859b
anyubinn <anyubin0724@naver.com>
2025-04-10 14:21:37 +0900

    Feat/#40: 댓글 좋아요 수 조회 기능 추가

--- src/main/java/com/sns/api/likes/controller/CommentLikesController.java
+++ src/main/java/com/sns/api/likes/controller/CommentLikesController.java
@@ -1,6 +1,7 @@
 package com.sns.api.likes.controller;
 
 import com.sns.api.common.domain.dto.UserBaseDto;
+import com.sns.api.likes.domain.dto.LikeCountResponseDto;
 import com.sns.api.likes.domain.dto.LikeResponseDto;
 import com.sns.api.likes.domain.entity.LikeType;
 import com.sns.api.likes.service.LikesService;
@@ -9,6 +10,7 @@ import com.sns.common.component.Const;
 import com.sns.common.component.ResultCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +38,11 @@ public class CommentLikesController {
 
         likesService.deleteLike(commentId, LikeType.COMMENT, userBaseDto);
         return BaseResponse.success(null, ResultCode.NO_CONTENT);
+    }
+
+    @GetMapping("/count")
+    public BaseResponse<LikeCountResponseDto> countCommentLike(@PathVariable Long commentId) {
+
+        return BaseResponse.success(likesService.countLike(commentId, LikeType.COMMENT), ResultCode.OK);
     }
 }
